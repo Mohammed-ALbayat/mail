0b660875ca184b53792b89bd059b56a712e2c856
Mohammed-Albayat <mmboommb999@gmail.com>
2024-10-17 16:02:55 +0300

    View emails in Archive, Sent and Inbox pages

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -6,44 +6,50 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
   
-
-  document.querySelectorAll('#read').forEach((button) => {
-    console.log(button);
-    // button.addEventListener('click', read(button));
-  });
-  
-  // document.querySelectorAll('#archive').forEach((button) => {
-  //   addEventListener('click', archive(button));
-  // });
-  // By default, load the inbox
   load_mailbox('inbox');
-});
+    
+});  
+  
+function read(id) {
 
-function read(button) {
-  fetch(`/emails/${button.dataset.id}`)
+  fetch(`/emails/${id}`)
   .then(response => response.json())
   .then(email => {
-      // Print email
-      console.log(email);
-      
-      const sender = document.createElement('div');
-      sender.innerHTML = `From: ${email.sender}`;
-      const recipients = document.createElement('div');
-      recipients.innerHTML = `TO + ${email.recipients}`;
-      const subject = document.createElement('div');
-      subject.innerHTML = email.subject;
-      const timestamp = document.createElement('div');
-      timestamp.innerHTML = email.timestamp;
-      const body = document.createElement('div');
-      body.innerHTML = email.body;
+    document.querySelector('#email-view').innerHTML = `
+      <h3 class = "sender"> From: ${email.sender} </h3>
+      <h3 class = "subject"> Subject: ${email.subject} </h3>
+      <p class = "timestamp2"> ${email.timestamp} </p>
+      <hr>
+      <p class = "body"> ${email.body}
+    `;  
 
-      document.querySelector('#email-view').append(sender);
-
-      document.querySelector('#compose-view').style.display = 'none';
-      document.querySelector('#emails-view').style.display = 'none';
-      document.querySelector('#email-view').style.display = 'block';
+    document.querySelector('#compose-view').style.display = 'none';
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#email-view').style.display = 'block';
       
   });
+}
+
+function archive(id, archive){
+
+  console.log(archive);
+  
+  if(archive === true){
+    archive = false;
+  }
+  else{
+    archive = true
+  }
+
+
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: archive
+    })
+  })
+  
+  load_mailbox((archive === true) ? 'inbox' : 'archive');
 
 }
 
@@ -61,38 +67,42 @@ function compose_email() {
 }
 
 function addEmail(email) {
+
+  //Create a new email
   const mail = document.createElement('div');
   if(email.read)
     mail.classList.add("email", "read");
   else
     mail.classList.add("email", "unread");
-  const sender = document.createElement('div');
-  sender.className = "sender";
-  sender.innerHTML = email.sender;
-  const timestamp = document.createElement('div');
-  timestamp.className = "timestamp";
-  timestamp.innerHTML = email.timestamp;
-  const subject = document.createElement('div');
-  subject.className = "subject";
-  subject.innerHTML = email.subject;
-  const read = document.createElement('button');
-  read.innerHTML = "read";
-  read.dataset.id = email.id;
-  read.classList.add("btn", "btn-sm", "btn-outline-primary", "btn-read");
-  read.id = "read";
-  read.style.margin = "5px";
-  const archive = document.createElement('button');
-  archive.innerHTML = "archive";
-  archive.id = "archive";
-  archive.style.margin = "5px";
-  archive.classList.add("btn", "btn-sm", "btn-outline-primary");
-  archive.dataset.id = email.id;
-  mail.appendChild(sender);
-  mail.appendChild(timestamp);
-  mail.appendChild(subject);
-  mail.appendChild(archive);
-  mail.appendChild(read);
+  mail.innerHTML = `
+   <h4 class = "sender"> Sender: ${email.sender} </h4>
+   <h3 class = "subject"> Subject: ${email.subject} </h3>
+   <div class = "timestamp"> ${email.timestamp} </div>
+  `;
+  //Add necessary buttons (Read, Archive)
+  const read_btn = document.createElement('button');
+  read_btn.innerHTML = "read";
+  read_btn.classList.add("btn", "btn-sm", "btn-outline-primary", "btn-read");
+  read_btn.id = "read";
+  read_btn.style.margin = "5px";
 
+  const archive_btn = document.createElement('button');
+  if(email.archived){
+    archive_btn.innerHTML = "Unarchive";
+  }
+  else {
+    archive_btn.innerHTML = "Archive";
+  }
+  archive_btn.id = "archive";
+  archive_btn.style.margin = "5px";
+  archive_btn.classList.add("btn", "btn-sm", "btn-outline-primary");
+
+  mail.appendChild(archive_btn);
+  mail.appendChild(read_btn);
+  //Add eventListener to the buttons
+  read_btn.addEventListener('click', () => {read(email.id)});
+  archive_btn.addEventListener('click', () => {archive(email.id, email.archived)});
+  //Add created mail to emails view
   document.querySelector('#emails-view').append(mail);
 }
 
